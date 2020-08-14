82898807cc8f385e288e2445f04521c9636d4237
bfu4 <46942366+bfu4@users.noreply.github.com>
2020-08-14 01:35:06 -0400

    Logo file and fixed installation.

--- twitterhackv1/install.sh
+++ twitterhackv1/install.sh
@@ -2,8 +2,21 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd) " 
 cd $DIR
-chmod 777 twitterhack 
+
+### make twitterhack executable (you're welcome) ###
+chmod +x twitterhack
+
+### check for directory ###
+
+if [[ -d /usr/local/etc/twitterhack ]]; then
+  cp logo.txt /usr/local/etc
+  else
+    mkdir /usr/local/etc/twitterhack && mv logo.txt /usr/local/etc/twitterhack
+fi
+
+### twitterhack to the bin!!! :O ###
 cp twitterhack /usr/local/bin
-printf "\033[1;36m[*] Added twitterhack to bin! type: twitterhack\033[0m\n"
+### a message ###
+printf "\033[1;36m[*] Installed twitterhack! type: twitterhack\033[0m\n"
  
 
